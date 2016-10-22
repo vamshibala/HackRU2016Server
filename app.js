2a16a4652906fc7deeae0f7c4427971d97d30f9e
Harankumar Nallasivan <haranku16@gmail.com>
2016-10-22 17:17:40 -0400

    Changes

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express");
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var pollSearch = require('./poll-search.js');
+var RankingSchema = require('./ranking.js');
 var app = express();
 var port = 3000;
 
+var Ranking = mongoose.model('Ranking',RankingSchema);
 
+app.use(bodyParser);
 
 app.get("/", function(req, res){
 
@@ -21,3 +27,25 @@ app.listen(port, function(){
 
 // ONLY HARAN'S CHANGES PAST THIS POINT
 
+app.get('/REST/search', function(req,res){
+	res.send(pollSearch(req.body.keywords));
+});
+
+app.post('/REST/ranking', function(req,res){
+	// GET A RANKING JSON, STORE
+	var ranking = req.body;
+	Ranking.create(ranking,function(err,rankingFromDB){
+		if (err) res.sendStatus(404);
+		else {
+			res.send('OK');
+		}
+	});
+});
+
+app.get('/REST/poll', function(req,res){
+	
+});
+
+app.post('/REST/poll', function(req,res){
+	
+});
